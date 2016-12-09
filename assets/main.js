c23b941906b7e2e20cc2a3e51e75fb2752da30b4
Marcelo Bendahan <mbendahan@gmail.com>
2016-12-08 22:57:27 -0300

    Steps 5, 6, 7, 8, 9

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/switchuy.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      populateCources(response.courses.completed);
+    }
+  });
+
+  function populateCources(courses) {
+
+    $badges = $("#badges");
+
+    courses.forEach(function(course) {
+      $div = $('<div />')
+              .addClass('course')
+              .appendTo($badges);
+
+      $h3 = $('<h3 />')
+              .html(course.title)
+              .appendTo($div);
+
+      $img = $('<img />')
+              .attr('src', course.badge)
+              .appendTo($div);
+
+      $a = $('<a />')
+            .attr('href', course.url)
+            .addClass('btn btn-primary')
+            .html('See Course')
+            .appendTo($div);
+
+      $div.appendTo($badges);
+    })
+
+
+
+  }
 
 });
